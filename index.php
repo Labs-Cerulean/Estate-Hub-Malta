6a4631ddc302650c8252f3917de648ce428495e1
nicholasvpra <nicholasv@pramalta.com>
2025-12-30 15:36:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($_POST) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Estate Hub Malta</title>
-    <link rel="icon" href="logoicon.png">
+    <link rel="icon" href="logo_icon.png">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
