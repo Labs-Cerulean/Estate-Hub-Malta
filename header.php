056af8079da7cb175bd1cc917ec438e608c782c6
nicholasvpra <nicholasv@pramalta.com>
2026-01-06 21:47:41 +0100

    Update header.php

--- header.php
+++ header.php
@@ -41,5 +41,8 @@ requireLogin(); // Redirects if not logged in[file:10][file:3]
             <?php if (isAdmin()): ?>
                 <a href="users-management.php" class="nav-link">Users</a>
             <?php endif; ?>
+            <?php if (isAdmin() || getCurrentUserRole() === 'manager'): ?>
+                <li><a href="professionals-management.php" class="nav-link">Professionals</a></li>
+            <?php endif; ?>
         </div>
     </nav>
