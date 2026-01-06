a3d90ba467220f4c3da810c5caf32b9f4b6a40fd
nicholasvpra <nicholasv@pramalta.com>
2026-01-06 22:18:18 +0100

    Update professionals-management.php

--- professionals-management.php
+++ professionals-management.php
@@ -132,7 +132,7 @@ if (isset($_GET['edit'])) {
                         <button type="submit" class="btn"><?= $editing ? 'Update Professional' : 'Add Professional' ?></button>
                         <?php if ($editing): ?>
                             <a href="professionals-management.php" class="btn btn--secondary">Cancel</a>
-                        <?php $editing=null;endif; ?>
+                        <?php endif; ?>
                     </div>
                 </form>
             </div>
