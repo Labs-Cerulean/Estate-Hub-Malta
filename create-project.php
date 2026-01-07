258bf32ebc39c11edf4b6edc988e87386ae3a9b1
nicholasvpra <nicholasv@pramalta.com>
2026-01-07 18:25:08 +0100

    Update create-project.php

--- create-project.php
+++ create-project.php
@@ -100,15 +100,9 @@ $pageTitle = 'Create Project';
 require_once 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Create Project - Estate Hub</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
+
     <div class="main-container">
         <h1 class="page-title">Create New Project</h1>
         
@@ -339,5 +333,4 @@ require_once 'header.php';
             }
         });
     </script>
-</body>
-</html>
+<?php require_once 'footer.php'; ?>
