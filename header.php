d53184b389b042aa0315fdd14734d5a4cf68f4c8
nicholasvpra <nicholasv@pramalta.com>
2026-01-07 18:11:25 +0100

    Update header.php

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ $currentPage = basename($_SERVER['PHP_SELF'], '.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle) ?> - Estate Hub</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
     <?php if (isLoggedIn()): ?>
