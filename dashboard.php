e65450a34658aebb39eb63e7cfef683c502a712e
nicholasvpra <nicholasv@pramalta.com>
2026-01-08 15:40:16 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -503,9 +503,9 @@ require_once 'header.php';
     <div class="projects-header">
       <h2 class="section-title">Projects</h2>
       <span></span>
-      <?php if (isAdmin() || getCurrentRole() == 'manager') { ?>
+      <?php if (isAdmin() || getCurrentRole() == 'manager')  ?>
             <a href="create-project.php" class="btn">Add Project</a>
-        <?php } endif; ?>
+        <?php  endif; ?>
     </div>
 
     <!-- Filters Section -->
