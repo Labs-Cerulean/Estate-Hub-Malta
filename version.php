d97f592072d677d2dfdc5240b6e947bc510a4a4f
nicholasvpra <nicholasv@pramalta.com>
2026-01-09 17:49:21 +0100

    Create version.php

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+// version.php - Update this file during deployment
+return [
+    'branch' => 'main',  // or 'staging'
+    'commit' => 'abc1234',
+    'date' => '2026-01-09',
+    'version' => '1.0.0'
+];
