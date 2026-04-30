7b776fc5aba935b6d0aa9ad1ced3797eb86272fa
nicholasvpra <nicholasv@pramalta.com>
2026-04-30 09:41:50 +0200

    Update apit_test.php

--- apit_test.php
+++ apit_test.php
@@ -8,7 +8,7 @@ if (!in_array($_SESSION['role'], ['admin', 'system_manager'])) {
 }
 
 // 1. Set your credentials
-$apiKey = 'PASTE_YOUR_API_KEY_HERE';
+$apiKey = 'o/7b6jY815wajiIhCBbvd69etum9GykU5IX1LSG9Zfs=';
 $companyId = 'IBS999PRA';
 
 // 2. Define the endpoint. 
